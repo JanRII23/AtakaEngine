c7c4e3d1b5e1099e7acc512d849805769386a183
JRII_Ataka <jrejasii@gmail.com>
2026-02-07 17:26:28 -0600

    G12 : more changes tied to texturing vertex updates

--- DirectXGame/DX3D/Assets/Shaders/Basic.hlsl
+++ DirectXGame/DX3D/Assets/Shaders/Basic.hlsl
@@ -1,15 +1,17 @@
 struct VSInput
 {
     float3 position : POSITION0;
-    float4 color : COLOR0;
-    float4 color1 : COLOR1;
+    float2 texcoord : TEXCOORD0;
+//    float4 color : COLOR0;
+//  float4 color1 : COLOR1;
 };
 
 struct VSOutput
 {
     float4 position : SV_Position;
-    float4 color : COLOR0;
-    float4 color1 : COLOR1;
+    float2 texcoord : TEXCOORD0;
+//    float4 color : COLOR0;
+//    float4 color1 : COLOR1;
 };
 
 cbuffer constant : register(b0)
@@ -34,13 +36,13 @@ VSOutput VSMain(VSInput input)
     //SCREEN SPACE
     output.position = mul(output.position, m_proj);
     
-    output.color = input.color;
-    output.color1 = input.color1;
+    output.texcoord = input.texcoord;
+    //output.color1 = input.color1;
     return output;
 }
 
 
 float4 PSMain(VSOutput input) : SV_Target
 {
-    return lerp(input.color, input.color1, (sin(m_time / 1000.0f) + 1.0f) / 2.0f);
+    return lerp((sin(m_time / 1000.0f) + 1.0f) / 2.0f);
 }
--- DirectXGame/DX3D/Source/DX3D/Graphics/GraphicsEngine.h
+++ DirectXGame/DX3D/Source/DX3D/Graphics/GraphicsEngine.h
@@ -3,6 +3,7 @@
 #include <DX3D/Core/Base.h>
 #include <DX3D/Math/Vec3.h>
 #include <DX3D/Math/Vec4.h>
+#include <DX3D/Math/Vector2D.h>
 #include <Windows.h>
 #include <DX3D/Core/InputListener.h>
 #include <DX3D/Core/IFocusListener.h>
@@ -40,9 +41,10 @@ namespace dx3d
 	private:
 		struct Vertex
 		{
-			Vec3 position;
-			Vec4 color;
-			Vec4 color1;
+			Vector3D position;
+			Vector2D texcoord;
+			/*Vec4 color;
+			Vec4 color1;*/
 		};
 	private:
 		std::shared_ptr<GraphicsDevice> m_graphicsDevice{};
