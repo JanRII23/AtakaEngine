9db40a8716cc6325d600f8a34a4b1e29fa265d25
JRII_Ataka <jrejasii@gmail.com>
2026-04-01 22:42:41 -0500

    G13 : continuing work on mesh.cpp file and infra

--- DirectXGame/DX3D/Source/DX3D/Graphics/ResourceManager/MeshManager/Mesh.cpp
+++ DirectXGame/DX3D/Source/DX3D/Graphics/ResourceManager/MeshManager/Mesh.cpp
@@ -3,8 +3,10 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
 
-#include <locale>
-#include <codecvt>
+#include <windows.h>
+#include <string>
+
+#include <DX3D/Math/VertexMesh.h>
 
 using namespace dx3d;
 
@@ -17,7 +19,9 @@ dx3d::Mesh::Mesh(const wchar_t* full_path, GraphicsDevice& device) : Resource(fu
 	std::string warn;
 	std::string err;
 
-	std::string inputfile = std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(full_path);
+	int size_needed = WideCharToMultiByte(CP_UTF8, 0, full_path, -1, nullptr, 0, nullptr, nullptr);
+	std::string inputfile(size_needed - 1, '\0');
+	WideCharToMultiByte(CP_UTF8, 0, full_path, -1, &inputfile[0], size_needed, nullptr, nullptr);
 
 	bool res = tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, inputfile.c_str());
 
@@ -27,25 +31,37 @@ dx3d::Mesh::Mesh(const wchar_t* full_path, GraphicsDevice& device) : Resource(fu
 
 	if (shapes.size() > 1) throw std::exception("Mesh not create successfully");
 
+	std::vector<VertexMesh> list_vertices;
+
 	for (size_t s = 0; s < shapes.size(); s++) 
 	{
 		size_t index_offset = 0;
-		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+		list_vertices.reserve(shapes[s].mesh.indices.size());
+
+		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) 
+		{
 			unsigned char num_face_verts = shapes[s].mesh.num_face_vertices[f];
 
-			for (unsigned char v = 0; v < num_face_verts; v++) {
+			for (unsigned char v = 0; v < num_face_verts; v++) 
+			{
 				tinyobj::index_t index = shapes[s].mesh.indices[index_offset + v];
-				tinyobj::real_t vx = attribs.vertices[3 * index.vertex_index + 0];
-				tinyobj::real_t vy = attribs.vertices[3 * index.vertex_index + 1];
-				tinyobj::real_t vz = attribs.vertices[3 * index.vertex_index + 2];
+
+				tinyobj::real_t vx = attribs.vertices[index.vertex_index * 3 + 0];
+				tinyobj::real_t vy = attribs.vertices[index.vertex_index * 3 + 1];
+				tinyobj::real_t vz = attribs.vertices[index.vertex_index * 3 + 2];
 
 				tinyobj::real_t tx = attribs.texcoords[index.texcoord_index * 2 + 0];
 				tinyobj::real_t ty = attribs.texcoords[index.texcoord_index * 2 + 1];
+
+				VertexMesh vertex(Vector3D(vx, vy, vz), Vector2D(tx, ty));
+				list_vertices.push_back(vertex);
 			}
 
 			index_offset += num_face_verts;
 		}
 	}
+
+	device.createVertexBuffer(&list_vertices[0], sizeof(VertexMesh), list_vertices.size(), );
 }
 
 dx3d::Mesh::~Mesh()
--- DirectXGame/DX3D/Source/DX3D/Graphics/ResourceManager/MeshManager/Mesh.h
+++ DirectXGame/DX3D/Source/DX3D/Graphics/ResourceManager/MeshManager/Mesh.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <DX3D/Graphics/ResourceManager/Resource.h>
 #include <d3d11.h>
+#include <DX3D/Graphics/VertexBuffer.h>
+#include <DX3D/Graphics/IndexBuffer/IndexBuffer.h>
 
 namespace dx3d
 {
@@ -10,6 +12,8 @@ namespace dx3d
 		Mesh(const wchar_t* full_path, GraphicsDevice& device);
 		~Mesh();
 	private:
+		VertexBuffer m_vertex_buffer;
+		IndexBuffer m_index_buffer;
 		ID3D11Device* m_device = nullptr;
 		friend class DeviceContext;
 	};
